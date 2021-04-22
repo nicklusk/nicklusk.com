25a541dcec3f5e5515fee8413432b13889f6a5b2
Nick Lusk <r.nick.lusk@gmail.com>
2021-04-22 14:55:37 -0400

    Cleanup of minor issues and added github link in social

--- process_form.php
+++ process_form.php
@@ -11,11 +11,12 @@ $mail_body = "Name : ".$name."<br>";
 $mail_body .= "Email : " . $email . "<br>";
 $mail_body .= "Subject : " . $subject . "<br>";
 $mail_body .= "Message : " . $message . "<br>";
-if(mail("r.nick.lusk@gmail.com","From: Contact Form Nick Lusk",$mail_body,$headers)){
-    $thanks_mail_body = "Hello " . $name ."<br>";
+if(mail("r.nick.lusk@gmail.com"," " . $name ." - via Contact Form Nick Lusk",$mail_body,$headers)){
+    $thanks_mail_body = "Hey " . $name ."!<br>";
     
-    $thanks_mail_body .= "Hey! Thanks for your email!<br>";
-    $thanks_mail_body .= "I will get back to you soon.<br>";
+    $thanks_mail_body .= "This means your message made it, fantastic news!<br>";
+    $thanks_mail_body .= "I am delighted that you have taken the time to message me.<br>";
+    $thanks_mail_body .= "As soon as I reasonably can I will be back to you.<br>";
     $thanks_mail_body .= "Thank You,<br>";
     $thanks_mail_body .= "Nick Lusk";
 
